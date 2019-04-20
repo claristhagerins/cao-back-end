2cacef70ec04dbe07f508b4f97dd342f05128721
claristhagerins <claristhagerinsya22@gmail.com>
2019-04-20 14:48:10 +0700

    a

--- src/main/java/com/example/greeting/GreetingController.java
+++ src/main/java/com/example/greeting/GreetingController.java
@@ -9,11 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
-@RequestMapping("/greeting")
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class GreetingController {
 
-    @RequestMapping("")       
+    @RequestMapping("/greeting")       
     public String greeting() {
         return "ss";
     }
